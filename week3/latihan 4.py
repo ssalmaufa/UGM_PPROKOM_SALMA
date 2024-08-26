b47def5343fa4dca7554f777977a4bfe03af23ad
ssalmaufa <salmaaufaabdillah@mail.ugm.ac.id>
2024-08-26 14:24:25 +0700

    Create latihan 4.py

---
+++ week3/latihan 4.py
@@ -0,0 +1,9 @@
+str="Py'thon"
+str
+str2='Py''thon'
+str2
+"\"OK,\"sampai ketemu lagi."
+teks="Python adalah bahasa pemograman yang powerfull.\nJadi,saatnya kita menggunakan Python sebagai bahasa pemograman\nsehari-hari.Salam Python Dahsyat!"
+print(teks)
+kata="""Python adalah bahasa pemograman yang powerfull.Terbukti Python bisa dijalankan di segala platform OS.Jadi,saatnya kita menggunakan Python sebagai bahasa pemograman sehari-hari.Salam Python Dahsyat!"""
+print(kata)
