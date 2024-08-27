928537e5dd8ae60bd328dc822b1e599425001512
ssalmaufa <salmaaufaabdillah@mail.ugm.ac.id>
2024-08-27 12:15:07 +0700

    Create latihan 7.py

---
+++ week3/latihan 7.py
@@ -0,0 +1,21 @@
+z=['1','3','2','3','Alice','Bob']
+z.sort()
+print(2)
+print("Hello there!\nHow are you?\nI\'m doing fine")
+
+multi_line="""Hello there!
+How are you?
+I'm fine."""
+print(multi_line)
+
+spam='Hello Word'
+print(spam.strip())
+print(spam.lstrip())
+print(spam.rstrip())
+
+print(','.join (['cats','rats','bats']))
+print(''.join(['My','name','is','Simon']))
+print('ABC'.join(['My','name','is','Simon']))
+print('My name is simon'.split())
+print('MyABCnameABCisABCSimon'.split("ABC"))
+print('My name is Simon'.split('m'))
