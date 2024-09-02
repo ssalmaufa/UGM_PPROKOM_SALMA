53b3dae198947d53c3bcb8556140a8e8b205a6b6
ssalmaufa <salmaaufaabdillah@mail.ugm.ac.id>
2024-09-02 14:56:34 +0700

    Create latihan 5.py

---
+++ week 4/latihan 5.py
@@ -0,0 +1,39 @@
+name=str(input("enter student name:"))
+score=int(input("enter student score:"))
+
+if (score >=70):
+    print('lulus')
+else :
+    print('tidak lulus')
+    
+name=str(input("enter student name:"))
+score=int(input("enter student score:"))
+
+if (score >=70):
+    print('lulus')
+else :
+    print('tidak lulus')
+    
+name=str(input("enter student name:"))
+score=int(input("enter student score:"))
+
+if (score >=70):
+    print('lulus')
+else :
+    print('tidak lulus')
+    
+name=str(input("enter student name:"))
+score=int(input("enter student score:"))
+
+if (score >=70):
+    print('lulus')
+else :
+    print('tidak lulus')
+    
+name=str(input("enter student name:"))
+score=int(input("enter student score:"))
+
+if (score >=70):
+    print('lulus')
+else :
+    print('tidak lulus')
