094a8e6a89df4c0ea6c25e7b21439d0d436c4f14
salma aufa <salmaaufaabdillah@mail.ugm.ac.id>
2024-09-09 15:14:17 +0700

    Update latihan 6.py

--- week5/latihan 6.py
+++ week5/latihan 6.py
@@ -7,6 +7,7 @@ while(var_nilai<10):
     while(var_i<3):
         print("Perulangan ke",var_nilai,",",var_i)
         var_i+=1
+    var_i=1
     var_nilai+=1
 #diluar_perulangan var_nilai
 print("var_nilai=",int(var_nilai),"=10.Bernilai False")
