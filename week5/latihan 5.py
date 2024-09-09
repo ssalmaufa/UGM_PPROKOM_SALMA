27ec06d9f3cccf0287370e0525275b839534cdc3
salma aufa <salmaaufaabdillah@mail.ugm.ac.id>
2024-09-09 14:17:32 +0700

    Update latihan 5.py

--- week5/latihan 5.py
+++ week5/latihan 5.py
@@ -1,1 +1,12 @@
-
+#Deklarasi variabel
+var_nilai=0
+var_i=1
+#perulangan FOR
+for var_nilai in range (0,10):
+    print("Perulangan pertama Ke",var_nilai)
+    for var_i in range (1,3):
+        print("perulangan ke", var_nilai,",",var_i)
+#diluar perulangan var_i
+vari_i=1
+#diluar perulangan var_nilai
+print("var_nilai=", int(var_nilai)+1,"=10.Bernilai False")
