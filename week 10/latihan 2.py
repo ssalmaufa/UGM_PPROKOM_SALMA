2ede004a2a6aecf64fa7ba41be2add487c0c0032
salma aufa <salmaaufaabdillah@mail.ugm.ac.id>
2024-10-21 18:30:43 +0700

    Update latihan 2.py

--- week 10/latihan 2.py
+++ week 10/latihan 2.py
@@ -3,7 +3,7 @@ matriks = [[0, 0, 0], [0, 0, 0]]
 
 for i in range(2):  # Untuk baris
     for j in range(3):  # Untuk kolom
-        nilai = input(f"Masukkan nilai untuk matriks[{i}][{j}]: ")
+        nilai = int(input(f"Masukkan nilai untuk matriks[{i}][{j}]: "))
         matriks[i][j] = nilai
 
 
